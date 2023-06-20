a9ef60c30efec390163a49c7d1ab0f0407ce4ac5
pilar-am <pilimorenoperez@gmail.com>
2023-06-20 17:56:37 +0200

    Repositories and Services

--- BackEnd/tech4good/src/main/resources/data/load-data.bat
+++ BackEnd/tech4good/src/main/resources/data/load-data.bat
@@ -3,7 +3,7 @@ echo Beginning of insertion
 
 mongoimport --host localhost --port 27017 --db tech4good --collection commercialCensus --type json --file commercialCensus.json --jsonArray --drop
 mongoimport --host localhost --port 27017 --db tech4good --collection marketsFairs --type json --file mercats_i_fires.json --jsonArray --drop
-mongoimport --host localhost --port 27017 --db tech4good --collection marketsMun --type json --file mercats_mun.json --jsonArray --drop
+mongoimport --host localhost --port 27017 --db tech4good --collection marketsMun --type json --file mercats_municipals.json --jsonArray --drop
 mongoimport --host localhost --port 27017 --db tech4good --collection commercialGalleries --type json --file galerias_comerciales.json --jsonArray --drop
 mongoimport --host localhost --port 27017 --db tech4good --collection shoppingCenters --type json --file centres_comercials.json --jsonArray --drop
 
