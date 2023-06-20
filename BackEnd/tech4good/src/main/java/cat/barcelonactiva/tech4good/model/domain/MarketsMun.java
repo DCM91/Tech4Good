4cb4d6c4f95e0ab7e1a9f7c4a4d931a56ab3d017
montseliz <montse.liz92@gmail.com>
2023-06-20 17:09:40 +0200

    documents and jsons

---
+++ BackEnd/tech4good/src/main/java/cat/barcelonactiva/tech4good/model/domain/MarketsMun.java
@@ -0,0 +1,16 @@
+package cat.barcelonactiva.tech4good.model.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "marketsFairs")
+public class MarketsMun {
+
+
+
+}
